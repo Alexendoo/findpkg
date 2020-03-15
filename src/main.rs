77ac01a492064633b1f713928b0a1e9c9cf41c1b
Alex Macleod <alex@macleod.io>
2020-03-15 17:26:50 +0000

    Read .files tar archives

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use anyhow::{Context, Result};
+use once_cell::unsync::Lazy;
+use regex::{Match, Regex};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::PathBuf;
+use tar::{Archive, EntryType};
+use tempfile::TempDir;
+
+#[derive(Debug, Default)]
+struct Package {
+    desc: String,
+    files: String,
+}
+
+fn re(section: &str) -> Regex {
+    Regex::new(&format!(r"%{}%\n([^%]+)\n", section)).unwrap()
+}
+const NAME_RE: Lazy<Regex> = Lazy::new(|| re("NAME"));
+const FILES_RE: Lazy<Regex> = Lazy::new(|| re("FILES"));
+
+fn get_section<'a>(re: &Regex, text: &'a str) -> Option<Match<'a>> {
+    re.captures(text)?.get(1)
+}
+
+fn tempdir() -> io::Result<TempDir> {
+    tempfile::Builder::new()
+        .prefix(env!("CARGO_PKG_NAME"))
+        .tempdir()
+}
+
+fn read_packages(reader: impl Read) -> Result<HashMap<PathBuf, Package>> {
+    let mut archive = Archive::new(reader);
+    let mut packages = HashMap::<PathBuf, Package>::new();
+
+    for file in archive.entries()? {
+        let mut file = file?;
+
+        if file.header().entry_type() != EntryType::Regular {
+            continue;
+        }
+
+        let path = file.path()?;
+        let parent = path.parent().context("invalid parent")?.to_owned();
+        let file_name = path.file_name().context("invalid filename")?;
+
+        let package = packages.entry(parent).or_default();
+        let write = match file_name.to_str() {
+            Some("desc") => &mut package.desc,
+            Some("files") => &mut package.files,
+            _ => continue,
+        };
+        file.read_to_string(write)?;
+    }
+
+    Ok(packages)
+}
+
+fn main() -> Result<()> {
+    let core = File::open("./core.files")?;
+
+    let packages = read_packages(core)?;
+
+    for package in packages.values() {
+        println!("{}", package.desc)
+    }
+
+    Ok(())
+}
